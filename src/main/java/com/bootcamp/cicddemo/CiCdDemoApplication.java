a4508c9c70785e280cc40fffdf0fa0d7b7989dd0
monika <monikamahanthappa@gmail.com>
2021-09-16 07:31:20 +0530

    Initialise prooject with springboot and gradle

---
+++ src/main/java/com/bootcamp/cicddemo/CiCdDemoApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.cicddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiCdDemoApplication.class, args);
+	}
+
+}
